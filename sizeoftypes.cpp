60aa6f227d156f6641913ea36770d1700cedacaf
Praveen Venkatesh <praveenv253@gmail.com>
2014-01-09 20:40:00 +0530

    Add char to sizeoftypes.

--- sizeoftypes.cpp
+++ sizeoftypes.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+	cout << "sizeof(char): " << sizeof(char) << endl;
 	cout << "sizeof(short int): " << sizeof(short int) << endl;
 	cout << "sizeof(int): " << sizeof(int) << endl;
 	cout << "sizeof(long long): " << sizeof(long long) << endl;
@@ -11,6 +12,7 @@ int main()
 	cout << "sizeof(double): " << sizeof(double) << endl;
 	cout << "sizeof(long double): " << sizeof(long double) << endl;
 	cout << "sizeof(void *): " << sizeof(void *) << endl;
+	cout << "sizeof(char *): " << sizeof(char *) << endl;
 	cout << "sizeof(int *): " << sizeof(int *) << endl;
 	cout << "sizeof(float *): " << sizeof(float *) << endl;
 	cout << "sizeof(double *): " << sizeof(double *) << endl;
