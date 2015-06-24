05cddbb73aeadc9bd00f3c870270cd8eee16734c
Praveen Venkatesh <praveenv253@gmail.com>
2015-06-24 13:22:25 -0400

    Try a simpler indirection for condor.

--- htcondor/run_condor_prog.sh
+++ htcondor/run_condor_prog.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# Create an array out of the arguments
-args=( "$@" )
-
-# Quote all but the first argument
-# Note the usage of the array and slicing syntax. Refer:
-# - http://stackoverflow.com/questions/12711786/
-# - http://stackoverflow.com/questions/1335815/
-quoted_args=$(for arg in "${args[@]:1}"; do echo \"$arg\"; done)
-
-# Use the first argument as the program and run with the rest as arguments to
-# this program
-$1 $quoted_args
+"$@"
