3fea8b268e03acf7eabe7540c9e8935911394d3b
Praveen Venkatesh <praveenv253@gmail.com>
2023-09-04 21:32:12 -0700

    Add a test for CCA.
    - Commiting an old script: not sure if this is the right test exactly

--- sklearn/test_cca.py
+++ sklearn/test_cca.py
@@ -6,9 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg as la
 from scipy import stats
-from sklearn import preprocessing, svm
+from sklearn import linear_model
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
+from sklearn.cross_decomposition import CCA
 
 
 def gen_random_orthogonal_matrix(d):
@@ -45,49 +46,52 @@ def gen_data(n, d, c, p=0.5):
 
 
 if __name__ == '__main__':
+    #np.random.seed(120)
+
     num_total = 1000
     num_dims = 2
     max_classif_acc = 0.9
     (X, y, Q) = gen_data(num_total, num_dims, max_classif_acc)
 
-    #print(Q)
+    #np.random.seed(12)
+    #np.random.shuffle(y)
 
-    X = preprocessing.scale(X.T).T
+    #print(Q)
 
     plt.figure(figsize=(6, 6))
     plt.scatter(X[0, y==0], X[1, y==0], alpha=0.4)
     plt.scatter(X[0, y==1], X[1, y==1], alpha=0.4)
 
-    # classifer = svm.LinearSVC(fit_intercept=False)
-    classifer = svm.SVC(kernel='rbf')
-    # Cs = np.logspace(-4, 4, 25)
-    Cs = np.logspace(-2, 2, 5)
-    gammas = np.logspace(-2, 2, 5)
+    logistic = linear_model.LogisticRegression(fit_intercept=False)
+    Cs = np.logspace(-4, 4, 25)
     num_train = 700
 
-    pipe = Pipeline(steps=[('svc', classifer)])
-    estimator = GridSearchCV(pipe, dict(svc__C=Cs, svc__gamma=gammas), cv=5,
-                             n_jobs=5)
-    estimator.fit(X[:, :num_train].T, y[:num_train])
-    best_svc = estimator.best_estimator_.named_steps['svc']
+    cca = CCA(n_components=1)
+    cca.fit(X[:, :num_train].T, y)
+    print(X.shape)
+    print(y.shape)
 
-    y_hat_test = best_svc.predict(X[:, num_train:].T)
+    xo, yo = cca.transform(X[:, :num_train].T, y)
+
+    pipe = Pipeline(steps=[
+        ('cca', cca),
+        ('logistic', logistic),
+    ])
+    estimator = GridSearchCV(pipe, dict(logistic__C=Cs), cv=5, n_jobs=1)
+    estimator.fit(X[:, :num_train].T, y[:num_train])
+    best_logistic = estimator.best_estimator_.named_steps['logistic']
+
+    y_hat_test = best_logistic.predict(X[:, num_train:].T)
     acc = np.sum(y_hat_test == y[num_train:]) / (num_total - num_train) * 100
     print('Classification accuracy: %.3g%%' % acc)
 
-    # coef = np.squeeze(best_svc.coef_)
-    # print('Coeffs of estimated separator: ', end='')
-    # print(coef)
+    coef = np.squeeze(best_logistic.coef_)
+    print('Coeffs of estimated separator: ', end='')
+    print(coef)
 
-    # x1 = np.linspace(-6, 6, 2)
-    # x2 = - coef[0] * x1 / coef[1]
-    # plt.plot(x1, x2, 'k', linewidth=0.75)
-
-    x1, x2 = tuple(np.mgrid[-6:6:1000j, -6:6:1000j])
-    x1x2 = np.c_[x1.ravel(), x2.ravel()]
-    # x1x2 = preprocessing.scale(x1x2)
-    plt.contourf(x1, x2, best_svc.predict(x1x2).reshape((1000, 1000)),
-                 cmap=plt.cm.Greys, alpha=0.2)
+    x1 = np.linspace(-6, 6, 2)
+    x2 = - coef[0] * x1 / coef[1]
+    plt.plot(x1, x2, 'k', linewidth=0.75)
 
     plt.axis('equal')
     plt.xlim((-5, 5))
